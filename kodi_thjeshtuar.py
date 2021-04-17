a0650db82d031678a4b4e40bf8508543f08df340
adcodingacademy <82700860+adcodingacademy@users.noreply.github.com>
2021-04-17 10:22:05 +0200

    Create kodi_thjeshtuar.py

---
+++ kodi_thjeshtuar.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter import *
+dritare_kryesore = Tk()
+etiketë_teksti = Label(dritare_kryesore, text="Përshëndetje botë!")
+etiketë_teksti.pack()
+dritare_kryesore.mainloop()
